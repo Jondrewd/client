56f95640835c86383487a94d67ee550292929740
Jonas <jonas220703@gmail.com>
2025-04-23 18:51:48 -0300

    Ajustando rotas e importações
    Front end quase finalizado

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Book from './pages/Book';
 import Navbar from './components/Navbar/navbar';
+import Footer from './components/Footer/footer';
 import Inicio from './pages/Inicio/inicio';
-import Register from './pages/login/index2';
+import Register from './pages/Register/Register';
+import Profile from './pages/Profile/Profile';
+import Settings from './pages/Settings/Settings';
+import Highlights from './pages/Highlights/Highlight';
+import BookDetails from './pages/BookDetails/BookDetails';
 
-//UseHistory == useNavigate
 export default function AppRoutes(){
     return(
         <BrowserRouter>
@@ -16,8 +20,12 @@ export default function AppRoutes(){
                 <Route path='/login' exact element={<Login/>}/>  
                 <Route path='/register' exact element={<Register/>}/>    
                 <Route path='/books' exact element={<Book/>}/>    
-                
+                <Route path='/perfil' exact element={<Profile/>}/>    
+                <Route path='/settings' exact element={<Settings/>}/>  
+                <Route path='/destaques' exact element={<Highlights/>}/> 
+                <Route path='/bookDetails/:title' exact element={<BookDetails/>}/> 
             </Routes>
+            <Footer/>
         </BrowserRouter>
     )
 }
